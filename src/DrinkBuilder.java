7bb10a3ecba55c7b22a5bb37c474ce41de429f4d
Ярослав <hokjarosx@gmail.com>
2026-09-13 21:00:47 +0500

    interface DrinkBuilder

--- src/DrinkBuilder.java
+++ src/DrinkBuilder.java
@@ -1,3 +1,6 @@
 public interface DrinkBuilder {
-    DrinkBuilder milk
+    DrinkBuilder milk(String milk);
+    DrinkBuilder caneSugar(boolean caneSugar);
+    DrinkBuilder decoration(boolean decoration);
+    DrinkBuilder volume(int volume);
 }
