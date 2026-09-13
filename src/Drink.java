2b08df5aa8e36877ee3b2ea0afa2bc2d00e5b133
Ярослав <hokjarosx@gmail.com>
2026-09-13 21:02:33 +0500

    CoffeBuilder

--- src/Drink.java
+++ src/Drink.java
@@ -39,4 +39,32 @@ public class Drink {
             return this;
         }
     }
+
+    static class CoffeBuilder implements DrinkBuilder{
+        private String milk;
+        private boolean caneSugar;
+        private boolean decoration;
+        private int volume;
+
+        public DrinkBuilder milk(String milk){
+            this.milk = milk;
+            return this;
+        }
+
+        public DrinkBuilder caneSugar(boolean caneSugar){
+            this.caneSugar = caneSugar;
+            return this;
+        }
+
+        public DrinkBuilder decoration(boolean decoration){
+            this.decoration = decoration;
+            return this;
+        }
+
+        public DrinkBuilder volume(int volume){
+            this.volume = volume;
+            return this;
+        }
+    }
+
 }
